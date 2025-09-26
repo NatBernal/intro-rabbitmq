3dbe8a169e66b5e1f08da2fb8ca6f64fbce1adb5
NatBernal <nataliabernal.comoflo@gmail.com>
2025-09-25 20:44:34 -0500

    Formatt js files and add readme file

---
+++ README.md
@@ -0,0 +1,31 @@
+# Introducción a RabbitMQ
+
+Este proyecto es una introducción al uso de RabbitMQ, un sistema de mensajería que implementa el protocolo AMQP (Advanced Message Queuing Protocol).
+
+## ¿Qué se hizo?
+
+En este proyecto, se configuraron tres servicios principales utilizando Docker Compose:
+
+1. **RabbitMQ**: El servidor de mensajería que actúa como intermediario para el envío y recepción de mensajes.
+2. **Sender**: Un servicio que envía mensajes al servidor RabbitMQ.
+3. **Receiver**: Un servicio que recibe y procesa los mensajes desde RabbitMQ.
+
+# Flujos del tutorial
+
+## Sending (Envío de mensajes)
+
+En esta imagen el script send.js se conecta a RabbitMQ y envía un mensaje:
+![alt text](resources/run-sender.png)
+
+## Receiving (Recepción de mensajes)
+
+En esta imagen el script receive.js se conecta a RabbitMQ y espera mensajes de la cola hello:
+![alt text](resources/run-receiver.png)
+
+## Putting all together (Integración de todo)
+
+Finalemnte se levantan todos los servicios con Docker Compose:
+![alt text](resources/docker-compose.png)
+
+Al final de la ejecución ya se evidnecían los logs de ambos servicios
+![alt text](resources/evidence-MessageSent.png)
--- rabbitmq-tutorial/receive.js
+++ rabbitmq-tutorial/receive.js
@@ -1,28 +1,32 @@
-var amqp = require('amqplib/callback_api');
+var amqp = require("amqplib/callback_api");
 
-const host = process.env.RABBITMQ_HOST || "localhost";
-const port = process.env.RABBITMQ_PORT || "5672";
+const host = process.env.RABBITMQ_HOST;
+const port = process.env.RABBITMQ_PORT;
 const rabbitUrl = `amqp://${host}:${port}`;
 
-amqp.connect(rabbitUrl, function(error0, connection) {
-    if (error0) {
-        throw error0;
+amqp.connect(rabbitUrl, function (error0, connection) {
+  if (error0) {
+    throw error0;
+  }
+  connection.createChannel(function (error1, channel) {
+    if (error1) {
+      throw error1;
     }
-    connection.createChannel(function(error1, channel) {
-        if (error1) {
-            throw error1;
-        }
-        var queue = 'hello';
+    var queue = "hello";
 
-        channel.assertQueue(queue, {
-            durable: false
-        });
-
-        console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
-        channel.consume(queue, function(msg) {
-            console.log(" [x] Received %s", msg.content.toString());
-        }, {
-            noAck: true
-        });
+    channel.assertQueue(queue, {
+      durable: false,
     });
+
+    console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
+    channel.consume(
+      queue,
+      function (msg) {
+        console.log(" [x] Received %s", msg.content.toString());
+      },
+      {
+        noAck: true,
+      }
+    );
+  });
 });
